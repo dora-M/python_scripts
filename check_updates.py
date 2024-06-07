5a4d6bc089932ac745960a814141a0541ca728eb
dora-M <58212414+dora-M@users.noreply.github.com>
2024-06-07 08:26:48 +0300

    Update check_updates.py

--- check_updates.py
+++ check_updates.py
@@ -1,7 +1,47 @@
-# Import necessary modules
-import subprocess  # This module allows you to spawn new processes, connect to their input/output/error pipes, and obtain their return codes.
+import subprocess
+import logging
+import sys
+import re
 
-def check_updates():
+# Path to the OS release file
+os_release_file_path = '/etc/redhat-release'
+
+# Define a custom log level TRACE
+TRACE_LEVEL_NUM = 25
+# Log file
+LOG_FILE = 'file.log'
+
+
+def trace(self, message, *args, **kws):
+    if self.isEnabledFor(TRACE_LEVEL_NUM):
+        self._log(TRACE_LEVEL_NUM, message, args, **kws)
+
+
+# Extend the Logger class with the trace method
+class CustomLogger(logging.getLoggerClass()):
+    def trace(self, message, *args, **kws):
+        if self.isEnabledFor(TRACE_LEVEL_NUM):
+            self._log(TRACE_LEVEL_NUM, message, args, **kws)
+
+
+# Set the custom logger class
+logging.setLoggerClass(CustomLogger)
+
+# Add TRACE level to the logging module
+logging.addLevelName(TRACE_LEVEL_NUM, "TRACE")
+
+
+def configure_logging():
+    # Configure logging
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        handlers=[
+                            logging.FileHandler(LOG_FILE),
+                            logging.StreamHandler()
+                        ])
+
+
+def check_updates(logger):
     try:
         # Run the dnf check-update command and capture the output
         result = subprocess.run(['dnf', 'check-update'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -16,9 +56,11 @@ def check_updates():
         # Check the return code to determine if updates are available
         if result.returncode == 0:
             print(f"Operation was successful, No updates available.")
+            logger.info(f"Operation was successful, No updates available.")
             return False
         elif result.returncode == 1:
             print(f"An error occurred, which was handled by dnf.")
+            logger.error(f"An error occurred, which was handled by dnf.")
             return False
         elif result.returncode == 3:
             print(f"An unknown unhandled error occurred during operation.")
@@ -45,7 +87,16 @@ def check_updates():
 
 
 def main():
-    updates_available = check_updates()
+    # Call the function to configure logging and log messages
+    configure_logging()
+    # Create logger
+    logger = logging.getLogger(__name__)
+
+    # Log messages
+    logger.error("This is my error message")
+    logger.trace("This is my trace message")
+
+    updates_available = check_updates(logger)
     print(updates_available)
 
 
