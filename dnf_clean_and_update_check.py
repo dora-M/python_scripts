587ac71c58256f3a5a001577764f401dc3d92da1
dora-M <58212414+dora-M@users.noreply.github.com>
2024-06-13 08:37:11 +0300

    Update dnf_clean_and_update_check.py

--- dnf_clean_and_update_check.py
+++ dnf_clean_and_update_check.py
@@ -25,13 +25,13 @@ logging.CRITICAL 50 A serious error, indicating that the program itself may be u
     """
     logging.basicConfig(filename=log_file,
                         level=logging.INFO,
-                        format='%(asctime)s %(levelname)s: %(message)s')
+                        format="%(asctime)s %(levelname)s: %(message)s")
 
 
 def run_dnf_clean():
     try:
         # Run the dnf clean all command and capture the output
-        result = subprocess.run(['dnf', 'clean', 'all'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = subprocess.run(["dnf", "clean", "all"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         stdout_message = result.stdout.decode()
         stderr_message = result.stderr.decode()
@@ -44,41 +44,41 @@ def run_dnf_clean():
 
         # Check the return code to determine if updates are available
         if result.returncode == 0:
-            message = f'dnf clean all: Operation was successful.'
+            message = f"dnf clean all: Operation was successful."
             logging.info(message)
         elif result.returncode == 1:
-            message = (f'dnf clean all: An error occurred, which was handled by dnf.'
-                       f'\nSTDOUT: {stdout_message}'
-                       f'\nSTDERR: {stderr_message}')
+            message = (f"dnf clean all: An error occurred, which was handled by dnf."
+                       f"\nSTDOUT: {stdout_message}"
+                       f"\nSTDERR: {stderr_message}")
             logging.error(message)
         elif result.returncode == 3:
-            message = (f'dnf clean all: An unknown unhandled error occurred during operation.'
-                       f'\nSTDOUT: {stdout_message}'
-                       f'\nSTDERR: {stderr_message}')
+            message = (f"dnf clean all: An unknown unhandled error occurred during operation."
+                       f"\nSTDOUT: {stdout_message}"
+                       f"\nSTDERR: {stderr_message}")
             logging.critical(message)
         elif result.returncode == 200:
-            message = f'dnf clean all: There was a problem with acquiring or releasing of locks.'
+            message = f"dnf clean all: There was a problem with acquiring or releasing of locks."
             logging.warning(message)
         else:
-            message = (f'dnf clean all: Unexpected return code: {result.returncode}'
-                       f'\nSTDOUT: {stdout_message}'
-                       f'\nSTDERR: {stderr_message}')
+            message = (f"dnf clean all: Unexpected return code: {result.returncode}"
+                       f"\nSTDOUT: {stdout_message}"
+                       f"\nSTDERR: {stderr_message}")
             logging.error(message)
     except subprocess.CalledProcessError as e:
         # Print any error that occurs during the subprocess call
-        message = f'dnf clean all: An error occurred while dnf clean call: {e.stderr.strip()}'
+        message = f"dnf clean all: An error occurred while dnf clean call: {e.stderr.strip()}"
         logging.error(message)
     except subprocess.TimeoutExpired as e:
         # Handle timeout exception
-        message = f'dnf clean all: The command timed out: {e.stderr.strip() if e.stderr else "No stderr"}'
+        message = f"dnf clean all: The command timed out: {e.stderr.strip() if e.stderr else 'No stderr'}"
         logging.error(message)
     except subprocess.SubprocessError as e:
         # Handle other subprocess errors
-        message = f'dnf clean all: A subprocess error occurred: {str(e)}'
+        message = f"dnf clean all: A subprocess error occurred: {str(e)}"
         logging.error(message)
     except Exception as e:
         # Handle any other exceptions that occur during the subprocess run
-        message = f'dnf clean all: An error occurred while running dnf clean: {str(e)}'
+        message = f"dnf clean all: An error occurred while running dnf clean: {str(e)}"
         logging.critical(message)
 
 
@@ -89,26 +89,26 @@ def remove_all_dnf_cache():
     """
     try:
         subprocess.run(["rm", "-rf", "/var/cache/dnf/*"], check=True)
-        message = f'dnf clear cache: Successfully removed all files in /var/cache/dnf")'
+        message = f"dnf clear cache: Successfully removed all files in /var/cache/dnf"
         logging.info(message)
     except subprocess.CalledProcessError as e:
-        message = f'dnf clear cache: Failed to remove files: {e.stderr.strip() if e.stderr else str(e)}'
+        message = f"dnf clear cache: Failed to remove files: {e.stderr.strip() if e.stderr else str(e)}"
         logging.error(message)
     except subprocess.TimeoutExpired as e:
         message = f"dnf clear cache: Command timed out: {e.stderr.strip() if e.stderr else 'No stderr'}"
         logging.error(message)
     except subprocess.SubprocessError as e:
-        message = f'dnf clear cache: A subprocess error occurred: {str(e)}'
+        message = f"dnf clear cache: A subprocess error occurred: {str(e)}"
         logging.error(message)
     except Exception as e:
-        message = f'dnf clear cache: An unexpected error occurred: {str(e)}'
+        message = f"dnf clear cache: An unexpected error occurred: {str(e)}"
         logging.critical(message)
 
 
 def are_updates_available():
     try:
         # Run the dnf check-update command and capture the output
-        result = subprocess.run(['dnf', 'check-update'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = subprocess.run(["dnf", "check-update"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         stdout_message = result.stdout.decode()
         stderr_message = result.stderr.decode()
@@ -122,45 +122,45 @@ def are_updates_available():
 
         # Check the return code to determine if updates are available
         if result.returncode == 0:
-            message = f'dnf check-update: Operation was successful, No updates available.'
+            message = f"dnf check-update: Operation was successful, No updates available."
             logging.info(message)
         elif result.returncode == 1:
-            message = (f'dnf check-update: An error occurred, which was handled by dnf.'
-                       f'\nSTDOUT: {stdout_message}'
-                       f'\nSTDERR: {stderr_message}')
+            message = (f"dnf check-update: An error occurred, which was handled by dnf."
+                       f"\nSTDOUT: {stdout_message}"
+                       f"\nSTDERR: {stderr_message}")
             logging.error(message)
         elif result.returncode == 3:
-            message = (f'dnf check-update: An unknown unhandled error occurred during operation.'
-                       f'\nSTDOUT: {stdout_message}'
-                       f'\nSTDERR: {stderr_message}')
+            message = (f"dnf check-update: An unknown unhandled error occurred during operation."
+                       f"\nSTDOUT: {stdout_message}"
+                       f"\nSTDERR: {stderr_message}")
             logging.critical(message)
         elif result.returncode == 100:
-            message = f'dnf check-update: Updates are available.'
+            message = f"dnf check-update: Updates are available."
             logging.info(message)
             return True
         elif result.returncode == 200:
-            message = f'dnf check-update: There was a problem with acquiring or releasing of locks.'
+            message = f"dnf check-update: There was a problem with acquiring or releasing of locks."
             logging.warning(message)
         else:
-            message = (f'dnf check-update: Unexpected return code: {result.returncode}'
-                       f'\nSTDOUT: {stdout_message}'
-                       f'\nSTDERR: {stderr_message}')
+            message = (f"dnf check-update: Unexpected return code: {result.returncode}"
+                       f"\nSTDOUT: {stdout_message}"
+                       f"\nSTDERR: {stderr_message}")
             logging.error(message)
     except subprocess.CalledProcessError as e:
         # Print any error that occurs during the subprocess call
-        message = f'dnf check-update: An error occurred while checking for updates call: {e.stderr.strip()}'
+        message = f"dnf check-update: An error occurred while checking for updates call: {e.stderr.strip()}"
         logging.error(message)
     except subprocess.TimeoutExpired as e:
         # Handle timeout exception
-        message = f'dnf check-update: The command timed out: {e.stderr.strip() if e.stderr else "No stderr"}'
+        message = f"dnf check-update: The command timed out: {e.stderr.strip() if e.stderr else 'No stderr'}"
         logging.error(message)
     except subprocess.SubprocessError as e:
         # Handle other subprocess errors
-        message = f'dnf check-update: A subprocess error occurred: {str(e)}'
+        message = f"dnf check-update: A subprocess error occurred: {str(e)}"
         logging.error(message)
     except Exception as e:
         # Handle any other exceptions that occur during the subprocess run
-        message = f'dnf check-update: An error occurred while checking for updates: {str(e)}'
+        message = f"dnf check-update: An error occurred while checking for updates: {str(e)}"
         logging.critical(message)
     finally:
         return False
